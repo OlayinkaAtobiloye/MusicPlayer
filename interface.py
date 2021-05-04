71f9274d544ad9506c76af76da25a2db471e6e6f
Olayinka <hiqmatolayinka@gmail.com>
2021-05-04 19:53:07 +0100

    added music database as a feature

--- interface.py
+++ interface.py
@@ -4,25 +4,63 @@ import fnmatch
 import tkinter as tk
 import pygame
 import sqlite3
+from tkinter import *
 
 
 class Interface:
 
-
     def __init__(self):
         MusicInterface = tk.Tk()
         MusicInterface.title('mp3 player')
-        MusicInterface.geometry('600x400')
+        MusicInterface.geometry('600x500')
         pygame.init()
         pygame.mixer.init()
 
-        PlaylistFrame = tk.Listbox(MusicInterface, width=600, height=400)
-        PlaylistFrame.pack(fill=tk.BOTH, expand=True)
+        con = sqlite3.connect("general.db")
+        MusicPlaylist = con.cursor()
+        MusicPlaylist.execute('''CREATE TABLE IF NOT EXISTS General (filepath)''')
+
+        MusicFrame = Frame(MusicInterface)
+
+        NewPlaylist = Entry(MusicFrame)
+        NewPlaylist.pack(fill=X, side=LEFT)
+
+        def createplaylist():
+            MusicPlaylist.execute('''CREATE TABLE IF NOT EXISTS {} (filepath)'''.format(NewPlaylist.get()))
+            NewPlaylist.delete(0, END)
+
+        new_playlist = Button(MusicFrame, text='Create Playlist', command=createplaylist)
+        new_playlist.pack(fill=X, side=LEFT)
+
+        options = MusicPlaylist.execute("SELECT name FROM sqlite_master where type='table';")
+        options = options.fetchall()
+        variable = StringVar(MusicFrame)
+        variable.set(options[0])
+        dropdown = OptionMenu(MusicFrame, variable, *options)
+        dropdown.pack(fill=X, side=LEFT)
+
+        PlaylistFrame = tk.Listbox(MusicInterface, width=500, height=4)
         for path, directories, files in os.walk("\\users\\user pc\\Music\\"):
             absolutepath = os.path.abspath(path)
             for file in fnmatch.filter(files, '*.mp3'):
                 filepath = os.path.join(absolutepath, file)
                 PlaylistFrame.insert(tk.END, filepath)
+
+
+        def chooseplaylist():
+            PlaylistFrame.delete(0, END)
+            playlist = variable.get().split("'")[1]
+            songs = MusicPlaylist.execute('SELECT * FROM {};'.format(playlist))
+            # songs = MusicPlaylist.execute('SELECT * FROM General')
+            songs = songs.fetchall()
+            for song in songs:
+                PlaylistFrame.insert(END, song[0])
+
+        button2 = Button(text='Choose Playlist', master=MusicFrame, bg='red', command=chooseplaylist)
+        button2.pack(side=LEFT)
+
+        MusicFrame.pack(fill=X, expand=True)
+        PlaylistFrame.pack(fill=tk.BOTH, expand=True)
 
         ScrollBar = tk.Scrollbar(PlaylistFrame, orient=tk.VERTICAL, command=PlaylistFrame.yview)
         ScrollBar.pack(side=tk.RIGHT, fill=tk.BOTH)
@@ -34,6 +72,11 @@ class Interface:
             pygame.event.wait()
             while pygame.mixer.music.get_busy():
                 pygame.time.Clock()
+        def addtoplaylist():
+            song = PlaylistFrame.get(ACTIVE)
+            print(table.get().split("'")[1])
+            print(song)
+            MusicPlaylist.execute('INSERT INTO {0} VALUES(?);'.format(table.get().split("'")[1]), (song,))
 
         def pauseSong():
             pygame.mixer.music.pause()
@@ -52,37 +95,30 @@ class Interface:
             volume = pygame.mixer.music.get_volume() + 0.1
             pygame.mixer.music.set_volume(volume)
 
-        def nextSong():
-            currentSong = PlaylistFrame.get(tk.ACTIVE)
-            currentPosition = PlaylistFrame.index(currentSong)
-            nextPosition = currentPosition + 1
-            PlaylistFrame.select_clear(currentPosition)
-            PlaylistFrame.select_set(nextPosition)
-            # PlaylistFrame.selection_set(nextSong)
-
-        def previousSong():
-            currentSong = PlaylistFrame.get(tk.ACTIVE)
-            currentPosition = PlaylistFrame.index(currentSong)
-            previousPosition = currentPosition - 1
-            PlaylistFrame.activate(previousPosition)
 
         ControlFrame = tk.Frame(width=600, height=50)
-        PlayButton = tk.Button(text='Play', master=ControlFrame, command=playSong)
-        PlayButton.pack(fill=tk.X, side=tk.RIGHT, expand=True)
-        PauseButton = tk.Button(text='Pause', master=ControlFrame, command=pauseSong)
-        PauseButton.pack(fill=tk.X, expand=True, side=tk.RIGHT)
+        tables = MusicPlaylist.execute("SELECT name FROM sqlite_master where type='table';")
+        tables = tables.fetchall()
+        table = StringVar(ControlFrame)
+        table.set(tables[0])
+        tables_dropdown = OptionMenu(ControlFrame, table, *tables)
+        tables_dropdown.pack(fill=X, side=LEFT)
+        playbutton = tk.Button(text='Play', master=ControlFrame, command=playSong)
+        playbutton.pack(fill=tk.X, side=tk.RIGHT, expand=True)
+        pausebutton = tk.Button(text='Pause', master=ControlFrame, command=pauseSong)
+        pausebutton.pack(fill=tk.X, expand=True, side=tk.RIGHT)
         StopButton = tk.Button(text='Stop', master=ControlFrame, command=stopSong)
         StopButton.pack(fill=tk.X, expand=True, side=tk.RIGHT)
-        NextButton = tk.Button(text='Next', master=ControlFrame, command=nextSong)
-        NextButton.pack(fill=tk.X, expand=True, side=tk.RIGHT)
-        PreviousButton = tk.Button(text='Previous', master=ControlFrame, command=previousSong)
-        PreviousButton.pack(fill=tk.X, expand=True, side=tk.RIGHT)
         ReduceButton = tk.Button(text='Reduce Volume', master=ControlFrame, command=reduceVolume)
         ReduceButton.pack(fill=tk.X, expand=True, side=tk.RIGHT)
         IncreaseButton = tk.Button(text='Increase Volume', master=ControlFrame, command=increaseVolume)
         IncreaseButton.pack(fill=tk.X, expand=True, side=tk.RIGHT)
-        ControlFrame.pack(fill=tk.X, expand=True,)
+        addtoplaylistButton = tk.Button(text='Add to Playlist', master=ControlFrame, command=addtoplaylist, bg='blue')
+        addtoplaylistButton.pack(fill=tk.X, expand=True, side=tk.RIGHT)
+        ControlFrame.pack(fill=tk.X, expand=True)
         MusicInterface.mainloop()
+        con.commit()
+        con.close()
 
 
 Interface()
